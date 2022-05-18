cafdefa136bc5661871007ec9ce8bf5ed48e7ab2
nkyungm <97418768+nkyungm@users.noreply.github.com>
2022-05-18 15:10:31 +0900

    Add files via upload

--- TodoList/todo.js
+++ TodoList/todo.js
@@ -12,40 +12,43 @@ const createTodo = () => {
         if(e.key == 'Enter'){ // todoInputElement에 키보드 'enter'가 입력되었을 때
             content = todoInputElement.value; // todoInputElement의 내용을 가져와 
             pushTodo(content);// pushTodo(todoInputElement의 내용)한다. -> pushTodo() 함수는 바로 밑에 있습니다.
-            //todoInputElement.value="";// todoInputElement의 내용을 없앤다
+            todoInputElement.value="";// todoInputElement의 내용을 없앤다
         }
     });
 
     todoEnterBtn.addEventListener('click', (e)=>{ // todoEnterBtn의 'click'을 이벤트 리스너로 등록한다.
         content=todoInputElement.value; // todoInputElement의 내용을 가져와 
         pushTodo(content); // pushTodo(todoInputElement의 내용)한다. 
-        //todoInputElement.value="";// todoInputElement의 내용을 없앤다
+        todoInputElement.value="";// todoInputElement의 내용을 없앤다
     });
-	
-    todoInputElement.value="";// todoInputElement의 내용을 없앤다
+
+    //todoInputElement.value="";// todoInputElement의 내용을 없앤다 
 
 };
 
 const pushTodo = (content) => {
 	const newId = id++;
 
-	// newTodos에 기존 todos와 매개변수로 받아온 content를 객체로 만들어 넣는다.
-	
+	const newTodos=[...todos,{id:newId,content:content,isCompleted:false}];
+    // newTodos에 기존 todos와 매개변수로 받아온 content를 객체로 만들어 넣는다.
+    //console.log(newTodos);
+
 	paintTodos();
 
 };
 
 const completeTodo = (todoId) => {
-	// newTodo에 현재 todos을 가져와서 map(삼항연산자)을 돌린다. -> (삼항연산자) todo.id랑 todoId가 같으면 isCompleted를 not(!) 처리한다
-	// newTodos를 todos로 한다
+	const newTodos = todos.map(todo => (todoId === todo.id ? {... todo, isCompleted:!todo.isCompleted} : todo ));
+    // newTodo에 현재 todos을 가져와서 map(삼항연산자)을 돌린다. -> (삼항연산자) todo.id랑 todoId가 같으면 isCompleted를 not(!) 처리한다
+	todos = newTodos;// newTodos를 todos로 한다
 
 	paintTodos();
 	
 };
 
 const deleteTodo = (todoId) => {
-	// newTOdos에 현재 todos를 가져와 filter(조건)를 돌린다 -> todo.id랑 todoId가 같지 않은 것만 가져온다
-	// newTodos를 todos로 한다
+    const newTodos=todos.filter(todo=>todo.id!==todoId);// newTOdos에 현재 todos를 가져와 filter(조건)를 돌린다 -> todo.id랑 todoId가 같지 않은 것만 가져온다
+	todos=newTodos;// newTodos를 todos로 한다
 
 	paintTodos();
 
@@ -72,10 +75,11 @@ const updateTodo = (e, todoId) =>{
  
 };
 
-
 const paintTodos = () => {
     todoList.innerHTML = null;
     todos.forEach(todo => paint(todo));
+
+    //console.log(todo);
 };
 
 const paint = (todo) => {
@@ -89,20 +93,25 @@ const paint = (todo) => {
     const checkBtn = document.createElement('button');
     checkBtn.classList.add('checkbox');
     checkBtn.innerHTML = '✔︎';
-	// checkBtn에 이벤트 리스너로 'click'을 감지한다. 실행할 함수는 completeTodo
+	
+    checkBtn.addEventListener('click',(e)=>{
+        completeTodo(todo.id);// checkBtn에 이벤트 리스너로 'click'을 감지한다. 실행할 함수는 completeTodo
+        //console.log(todo.id);
+    });
 
     const content = document.createElement('div');
     content.classList.add('content');
     content.innerHTML = todo.content;
     content.addEventListener('dblclick', (e)=> updateTodo(e, todo.id));
 
-
     const delBtn = document.createElement('button');
     delBtn.classList.add('delBtn');
     delBtn.innerHTML = '✕';
-	// delBtn에 이벤트 리스너로 'click'을 감지한다. 실행할 함수는 deleteTodo
-
-
+	
+    delBtn.addEventListener('click',(e)=>{
+        deleteTodo(todo.id);// delBtn에 이벤트 리스너로 'click'을 감지한다. 실행할 함수는 deleteTodo
+    });
+    
     liElement.appendChild(checkBtn);
     liElement.appendChild(inputElement);
     liElement.appendChild(content);
