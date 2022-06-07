e6b5625a84ae94312ee28bc9fc681eb31395e37c
nkyungm <skarudals27@naver.com>
2022-06-07 21:04:17 +0900

    react_quiz2

---
+++ react_quiz2/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useCallback} from "react";
+import Light from "./Light";
+
+
+export default function SmartHome() {
+    
+    const [masterOn, setMasterOn] = useState(false);
+    const [kitchenOn, setKitchenOn] = useState(false);
+    const [bathOn, setBathOn] = useState(false);
+
+	const toggleMaster = useCallback(() => setMasterOn(!masterOn), [masterOn]);
+    const toggleKitchen = useCallback(() => setKitchenOn(!kitchenOn),[kitchenOn]);
+    const toggleBath = useCallback(() => setBathOn(!bathOn), [bathOn]);
+
+    return (
+        <div className="App">
+            <Light room="침실" on={masterOn} toggle={toggleMaster} />
+            <Light room="주방" on={kitchenOn} toggle={toggleKitchen} />
+            <Light room="욕실" on={bathOn} toggle={toggleBath} />
+        </div>
+    );
+    
+}
